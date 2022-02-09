4edb40184625b865481a7115bb4e2452e4bfdb48
Rodrigo Mejia <rmejia.g36@gmail.com>
2022-02-08 20:27:10 -0800

    edited postion absolute

--- components/Unitspicker.js
+++ components/Unitspicker.js
@@ -15,8 +15,6 @@ export default function Unitspicker({unitsSystem, setUnitsSystem}) {
 
 const styles = StyleSheet.create({
     unitsSystem:{
-        position: 'absolute',
-
         ...Platform.select({
             ios:{
                 top: -30,
@@ -25,7 +23,7 @@ const styles = StyleSheet.create({
                 top: 30,
             }
         }),
-        
+        position: 'absolute',
         left: 20,
         height: 50,
         width: 100,
